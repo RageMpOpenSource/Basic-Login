8ae54707f85e5407c9725caa17c061feaacc60c0
$kylar <alleighteen@gmail.com>
2020-02-23 15:34:06 +0530

    Server-Sided files + Structure

---
+++ Chat/Colors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace Furious_V.Chat
+{
+    class Colors : Script
+    {
+        public static readonly String Color_White = "!{#FFFFFF}";
+        public static readonly String Color_Black = "!{#000000}";
+        public static readonly String Color_Info = "!{#A9C4E4}";
+        public static readonly String Color_Error = "!{#FF0000}";
+        public static readonly String Color_Admin = "!{#FFa500}";
+        public static readonly String Color_Developer = "!{#AB075F}";
+        public static readonly String Color_Helper = "!{#369988}";
+        public static readonly String Color_Newbie = "!{#87CEFA}";
+        public static readonly String COLOR_GRAD1 = "!{#B4B5B7}";
+        public static readonly String COLOR_GRAD2 = "!{#BFC0C2}";
+        public static readonly String COLOR_GRAD3 = "!{#CBCCCE}";
+        public static readonly String COLOR_GRAD4 = "!{#D8D8D8}";
+        public static readonly String COLOR_GRAD5 = "!{#E3E3E3}";
+        public static readonly String COLOR_GRAD6 = "!{#F0F0F0}";
+        public static readonly String COLOR_GRAD7 = "!{#C2A2DA}";
+    }
+}
---
+++ Chat/System.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Furious_V.Chat
+{
+    class Proximity
+    {
+    }
+}
