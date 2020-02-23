71929a228bf41cfe65646c2e606aa7c22c123861
$kylar <alleighteen@gmail.com>
2020-02-23 16:00:00 +0530

    Client-Side
    Still incomplete with the login.html
    Almost done

--- README.md
+++
@@ -1,6 +0,0 @@
-# Basic-Login
-This repository contains the basic methods of creating a extended class for a base class such as Client or Vehicle for the purpose of storing additional attributes for the entity. 
-It also represents most of the SQL functionolaties such as executing SQL queries, fetching data, updating data. 
-Also, it displays how to use CEFs for UI such as Login/Registration.
-
-More information will be stated in the Wiki page as to how to use the resource. As this will be a plug-and-play gamemode.
---
+++ client_packages/cs_packages/ClientLogin/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using RAGE;
+using RAGE.Ui;
+
+namespace ClientLogin
+{
+    public class Main : Events.Script
+    {
+        private HtmlWindow LoginPage = null;
+
+        public Main()
+        {
+            Events.Add("Login_PlayerLogin", PlayerLogin);
+            Events.Add("Login_LoginInfoToClient", LoginInfoToServer);
+            Events.Add("Login_EditWelcomeText", EditWelcomeText);
+            Events.Add("Login_EditButtonText", EditButtonText);
+        }
+
+        public void PlayerLogin(object [] args)
+        {
+            if ((bool)args[0])
+            {
+                LoginPage = new HtmlWindow("package://LoginPage.html");
+                ShowLoginPage(true);
+            }
+            else
+            {
+                if (LoginPage != null)
+                {
+                    ShowLoginPage(false);
+                    LoginPage.Destroy();
+                    LoginPage = null;
+                }                
+            }
+        }
+
+        public void LoginInfoToServer(object [] args)
+        {
+            Events.CallRemote("Login_LoginInfoToServer", (string)args[0]);
+        }
+
+        public void EditWelcomeText(object [] args)
+        {
+            if(LoginPage != null)
+            {
+                LoginPage.ExecuteJs($"EditWelcomeText('{args[0].ToString()}')");
+            }
+        }
+
+        public void EditButtonText(object[] args)
+        {
+            if (LoginPage != null)
+            {
+                LoginPage.ExecuteJs($"EditButtonText('{args[0].ToString()}')");
+            }
+        }
+
+        public void ShowLoginPage(bool show)
+        {
+            LoginPage.Active = show;
+            Cursor.Visible = show;
+            RAGE.Elements.Player.LocalPlayer.FreezePosition(show);
+        }
+    }
+}
