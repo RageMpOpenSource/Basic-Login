50d3f289f244a7881fe7be876f9442518a50ff19
$kylar <alleighteen@gmail.com>
2020-02-24 03:01:50 +0530

    notification html, login/registration html + .cs

--- client_packages/cs_packages/ClientLogin/Main.cs
+++ client_packages/cs_packages/ClientLogin/Main.cs
@@ -15,14 +15,18 @@ namespace ClientLogin
             Events.Add("Login_LoginInfoToClient", LoginInfoToServer);
             Events.Add("Login_EditWelcomeText", EditWelcomeText);
             Events.Add("Login_EditButtonText", EditButtonText);
+            Events.Add("Login_EditInformationText", EditInformationText);
         }
 
         public void PlayerLogin(object [] args)
         {
             if ((bool)args[0])
             {
-                LoginPage = new HtmlWindow("package://LoginPage.html");
-                ShowLoginPage(true);
+                if (LoginPage == null)
+                {
+                    LoginPage = new HtmlWindow("package://LoginPage.html");
+                    ShowLoginPage(true);
+                }
             }
             else
             {
@@ -53,6 +57,14 @@ namespace ClientLogin
             if (LoginPage != null)
             {
                 LoginPage.ExecuteJs($"EditButtonText('{args[0].ToString()}')");
+            }
+        }
+
+        public void EditInformationText(object [] args)
+        {
+            if (LoginPage != null)
+            {
+                LoginPage.ExecuteJs($"EditInformationText('{args[0].ToString()}')");
             }
         }
 
---
+++ client_packages/cs_packages/PopUpNotification/PopUpNotification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RAGE;
+using RAGE.Ui;
+
+namespace ClientLogin
+{
+    class PopUpNotification : Events.Script
+    {
+        private HtmlWindow Notification;
+
+        PopUpNotification()
+        {
+            Events.Add("Notification_PopUpNotification", Notification_PopUpNotification);
+            Events.Add("Notification_NotificationOff", Notification_NotificationOff);
+            Notification = new HtmlWindow("package://TopNotification.html");
+        }
+
+        private void Notification_PopUpNotification(object [] args)
+        {
+            Notification.Active = true;
+            Notification.ExecuteJs($"ExecuteNotification('{args[0].ToString()}', '{Convert.ToInt32(args[1])}', '{args[2].ToString()}', '{Convert.ToBoolean(args[3])}')");
+        }
+        private void Notification_NotificationOff(object [] args)
+        {
+            Notification.Active = false;
+        }
+    }
+}
