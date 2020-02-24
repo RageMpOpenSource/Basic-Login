9844404db8a290d272d391596a3633612764e527
$kylar <alleighteen@gmail.com>
2020-02-25 00:21:45 +0530

    /savechars | /saveall for Head admin+

--- Admin/Data.cs
+++ Admin/Data.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
+
+using GTANetworkAPI;
 
 namespace Furious_V.Admin
 {
     /// <summary>
     /// Contains all relevant attributes and methods for admins.
     /// </summary>
-    public class Data
+    public class Data : Script
     {
         /// <summary>
         /// The different types of admin.
@@ -19,6 +22,26 @@ namespace Furious_V.Admin
             ADMIN_HEAD,
             ADMIN_EXECUTIVE,
             ADMIN_OWNER
+        }
+        [Command("savechars", Alias = "saveall")]
+        public static async Task CMD_SaveChars(Client player)
+        {
+            if (Player.Data.GetPlayerData(player).Admin < E_ADMIN_LEVEL.ADMIN_HEAD)
+            {
+                return;
+            }
+            int count = 0;
+
+            foreach (Client item in NAPI.Pools.GetAllPlayers())
+            {
+                if (Player.Data.GetPlayerData(item).LoggedIn)
+                {
+                    await Player.Data.GetPlayerData(item).SavePlayerData(item);
+                    count++;
+                }
+            }
+            Utils.Log($"{count} player data were saved.", Utils.Log_Status.Log_Debug);
+            Utils.PopUpNotification(player, $"{count} player data were saved.", 3000, "lightgreen", true);
         }
     }
 }
